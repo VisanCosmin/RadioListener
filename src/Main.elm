c5eed58b473f6cbd279a5027b44e5de6e8ebb0f8
VisanCosmin <cozma.cozmyn@yahoo.com>
2020-11-18 14:47:38 +0200

    change url

--- src/Main.elm
+++ src/Main.elm
@@ -63,7 +63,7 @@ init jsonFavorite =
            , playerVolume = "0.5"
            } 
          , Http.get
-            { url = "http://www.radiolistener.net/stations.json"
+            { url = "https://visancosmin.github.io/Radio/stations.json"
             , expect = Http.expectJson RequestStationsResult (JD.list stationDecoder)
             } 
          )
